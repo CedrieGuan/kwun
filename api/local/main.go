317b11f051068cbd59c827b2e34f913da0ba1724
guanlingyu1 <guanlingyu1@shopline.com>
2025-12-19 18:33:06 +0800

    feat: update local API main entrypoint

--- api/local/main.go
+++ api/local/main.go
@@ -1,10 +1,8 @@
-package main
+package handler
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/joho/godotenv"
 	"github.com/kwun/onelink-api/lib"
 )
 
@@ -12,19 +10,4 @@ import (
 // Handler是Vercel调用的导出函数，用于无服务器部署
 func Handler(w http.ResponseWriter, r *http.Request) {
 	lib.Handler(w, r)
-}
-
-// main is used for local development only
-// main函数仅用于本地开发
-func main() {
-	if err := godotenv.Load(".env"); err != nil {
-		log.Println("No .env file found in .env, checking standard environment")
-	}
-
-	http.HandleFunc("/api/chat", lib.Handler)
-
-	log.Println("Local server starting on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
 }
